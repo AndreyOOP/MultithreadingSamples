4c9322eab49de07b27c0359b7fc0c96532db7bd9
abc <abc@gmail.com>
2022-01-08 15:57:04 +0200

    Part 1

---
+++ MultithreadingSamples/Tasks/ExceptionHndling.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples.Tasks
+{
+    [TestClass]
+    public class ExceptionHndling
+    {
+        [TestMethod]
+        public void SeemsNoException_SingleTask()
+        {
+            Task task;
+            try
+            {
+                task = Task.Factory.StartNew(() => throw new AccessViolationException()); // looks like no exception thrown
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void CatchAgregateException_SingleTaks()
+        {
+            Task task;
+            try
+            {
+                task = Task.Factory.StartNew(() => throw new AccessViolationException());
+                task.Wait(); // need to wait will result
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex is AggregateException);
+            }
+        }
+
+        [TestMethod]
+        public void HandleAggregateExceptions()
+        {
+            var t1 = Task.Factory.StartNew(() => throw new AccessViolationException());
+            var t2 = Task.Factory.StartNew(() => throw new InvalidCastException());
+
+            try
+            {
+                Task.WaitAll(t1, t2);
+            }
+            catch (AggregateException ex)
+            {
+                ex.Handle(e => {
+                    if (e is AccessViolationException)
+                    {
+                        Console.WriteLine("AccessViolationException handled");
+                        return true; // means the exception handled
+                    }
+                    if (e is InvalidCastException)
+                    {
+                        Console.WriteLine("InvalidCastException handled");
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+    }
+}
---
+++ MultithreadingSamples/Tasks/WaitingInTask.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples.Tasks
+{
+    [TestClass]
+    public class WaitingInTask
+    {
+        [TestMethod]
+        public void WaitSample()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = new Task(() =>
+            {
+                Thread.Sleep(1000); // scheduler need no processor time anymore => but switch context
+                Thread.SpinWait(10); // pause a thread, but scheduler is not going to get other task - CPU uses resources on this
+                SpinWait.SpinUntil(() => true); // same as above
+
+                bool cancelled = token.WaitHandle.WaitOne(3000); // will wait for 3 sec
+                if(cancelled) // check if it was cancelled cts.Cancel() or just WaitHandle.WaitOne pass
+                    Console.WriteLine("Cancelled");
+            }, token);
+            task.Start();
+
+            Console.ReadLine();
+            cts.Cancel();
+        }
+    }
+
+    [TestClass]
+    public class WaitForTask
+    {
+        [TestMethod]
+        public void WaitTaks()
+        {
+            var token = new CancellationTokenSource().Token;
+            var t1 = Task.Factory.StartNew(() => Thread.Sleep(2000));
+            var t2 = Task.Factory.StartNew(() => Thread.Sleep(2000));
+
+            t1.Wait(2000, token);
+            Task.WaitAll(t1, t2);
+            Task.WaitAll(new[] { t1, t2 }, token);
+            Task.WaitAny(new[] { t1, t2 }, 1000); // timeout
+        }
+    }
+}
