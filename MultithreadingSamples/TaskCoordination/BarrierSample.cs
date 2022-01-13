c30246727d90b5ae9b7f327a9b0e8f9c3308fde8
abc <abc@gmail.com>
2022-01-14 00:22:06 +0200

    Add barrier & countdown samples

---
+++ MultithreadingSamples/TaskCoordination/BarrierSample.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples.TaskCoordination
+{
+    [TestClass]
+    public class BarrierSample
+    {
+        Random random = new Random();
+        Barrier barrier = new Barrier(2, b => Console.WriteLine($"Phase {b.CurrentPhaseNumber} completed{Environment.NewLine}"));
+
+        // note that inside phases order changes from run to run
+        [TestMethod]
+        public void BarrierSampleTest()
+        {
+            var kettle = Task.Factory.StartNew(() => {
+                Step("Putting kettle");
+                //barrier.RemoveParticipant(); in case on the next phase required only one thread
+                Step("Puoring water");
+                Step("Puttin kettle away");
+            });
+            var cup = Task.Factory.StartNew(() => {
+                Step("Finding cup");
+                Step("Adding tea");
+                Step("Adding sugar");
+            });
+            Task.WaitAll(kettle, cup);
+        }
+
+        private void Step(string description)
+        {
+            Thread.Sleep(random.Next(1000));
+            Console.WriteLine(description);
+            barrier.SignalAndWait();
+        }
+    }
+}
---
+++ MultithreadingSamples/TaskCoordination/CountdownSample.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples.TaskCoordination
+{
+    [TestClass]
+    public class CountdownSample
+    {
+        [TestMethod]
+        public void CountdownSampleTest()
+        {
+            var random = new Random();
+            var cde = new CountdownEvent(5);
+            new[] { 1, 2, 3, 4, 5 }.ToList().ForEach(
+                i => Task.Factory.StartNew(() => {
+                    Thread.Sleep(random.Next(2000));
+                    Console.WriteLine($"Task {i} completed");
+                    cde.Signal(); // reduces internal countdown counter by one
+                })
+            );
+
+            var taskDoAfter = Task.Factory.StartNew( () =>
+            {
+                Console.WriteLine("Final task starting");
+                cde.Wait(); // block thread till counter become 0
+                Console.WriteLine("Final task completed");
+            });
+            taskDoAfter.Wait();
+        }
+    }
+}
