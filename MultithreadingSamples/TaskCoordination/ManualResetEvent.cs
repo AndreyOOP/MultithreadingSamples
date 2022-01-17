6a2c529cfa8fb1e52613b25659facd7b7c16d44f
abc <abc@gmail.com>
2022-01-17 22:58:30 +0200

    Reset event & semaphore

---
+++ MultithreadingSamples/TaskCoordination/ManualResetEvent.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples.TaskCoordination
+{
+    [TestClass]
+    public class ResetEventSample
+    {
+        [TestMethod]
+        public void ManualResetEventTest()
+        {
+            var resetEvent = new ManualResetEventSlim();
+            Task.Factory.StartNew(() => {
+                Console.WriteLine("Boiling water");
+                Thread.Sleep(1000);
+                resetEvent.Set(); // signaling that this step is complete
+            });
+            var task = Task.Factory.StartNew(() => {
+                resetEvent.Wait(); // waiting the singnal
+                resetEvent.Wait(); // few waits are ok
+                Console.WriteLine("Boiled");
+            });
+            task.Wait();
+        }
+
+        [TestMethod]
+        public void AutoResetEvent()
+        {
+            var resetEvent = new AutoResetEvent(false);
+            Task.Factory.StartNew(() => {
+                Console.WriteLine("Boiling water");
+                Thread.Sleep(1000);
+                resetEvent.Set(); // signaling that this step is complete
+            });
+            var task = Task.Factory.StartNew(() => {
+                resetEvent.WaitOne(); // waiting the singnal
+                // resetEvent.WaitOne(); // stack here, requiers next .Set()
+                Console.WriteLine("Boiled");
+            });
+            task.Wait();
+        }
+    }
+}
---
+++ MultithreadingSamples/TaskCoordination/SemaphoreSample.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples.TaskCoordination
+{
+    [TestClass]
+    public class SemaphoreSample
+    {
+        [TestMethod]
+        public void SemSample()
+        {
+            var semaphore = new SemaphoreSlim(2, 2);
+            for(int i=0; i<20; i++)
+            {
+                var temp = i;
+                Task.Factory.StartNew(() => {
+                    Console.WriteLine($"{temp} - start execution");
+                    semaphore.Wait(); // reduce internal counter
+                    Thread.Sleep(100);
+                    Console.WriteLine($"{temp} - complete execution");
+                });
+            }
+
+            for(int i=0; i<7; i++)
+            {
+                Task.Delay(500).Wait();
+                // allows next n threads to execute
+                // increase internal counter by 2 => two more tasks will be allowed to execute
+                var prev = semaphore.Release(2);
+                Console.WriteLine($"Semaphore count: previous = {prev}, current = {semaphore.CurrentCount}");
+            }
+        }
+    }
+}
