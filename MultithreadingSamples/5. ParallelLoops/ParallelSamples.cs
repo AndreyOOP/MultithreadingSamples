bdb398dacc440e35a20d5086cb986441d43c6752
abc <abc@gmail.com>
2022-01-18 00:02:42 +0200

    Rename folders

---
+++ MultithreadingSamples/5. ParallelLoops/ParallelSamples.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples._5._ParallelLoops
+{
+    [TestClass]
+    public class ParallelSamples
+    {
+        [TestMethod]
+        public void Invoke()
+        {
+            var t1 = new Action(() => Console.WriteLine("Task 1"));
+            var t2 = new Action(() => Console.WriteLine("Task 2"));
+            var t3 = new Action(() => Console.WriteLine("Task 3"));
+            var t4 = new Action(() => Console.WriteLine("Task 4"));
+
+            var options = new ParallelOptions
+            {
+                CancellationToken = default,
+                MaxDegreeOfParallelism = 5
+            };
+            // wrap actions into tasks
+            // blocking - wait till completed
+            Parallel.Invoke(options, t1, t2, t3, t4); 
+        }
+
+        [TestMethod]
+        public void ParallelFor()
+        {
+            // step is always 1, for anotehr step use foreach + custom step provider
+            Parallel.For(1, 11, i => Console.WriteLine($"{i * i}"));
+        }
+
+        [TestMethod]
+        public void ParallelForEach()
+        {
+            // as well allowed options
+            Parallel.ForEach(
+                new[] { "T1", "T2", "T3", "T4" },
+                s => Console.WriteLine($"Proceed {s}")
+            );
+        }
+    }
+}
---
+++ MultithreadingSamples/5. ParallelLoops/StoppingIteration.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples._5._ParallelLoops
+{
+    [TestClass]
+    public class StoppingIteration
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var cts = new CancellationTokenSource();
+            try
+            {
+                var options = new ParallelOptions
+                {
+                    CancellationToken = cts.Token
+                };
+                var result = Parallel.For(0, 20, options, (i, state) => {
+                    if(i == 10)
+                    {
+                        //state.Break(); // break iterations after 10 - here 0-10 task are executed
+                        //state.Stop(); // just stop execution ~ 3 tasks are executed
+                        //throw new Exception(); // similar to stop + prpagates AggregateException exception
+                        cts.Cancel(); // similar to stop + prpagates OperationCanceledException exception
+                    }
+                    Thread.Sleep(100);
+                    Console.WriteLine($"{i}[{Task.CurrentId}]");
+                });
+                Console.WriteLine($"{nameof(result.IsCompleted)}: {result.IsCompleted}");
+                if(result.LowestBreakIteration.HasValue)
+                    Console.WriteLine($"{nameof(result.LowestBreakIteration)}: {result.LowestBreakIteration.Value}");
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e => { 
+                    Console.WriteLine(e.Message);
+                    return true;
+                });
+            }
+            catch (OperationCanceledException ex) // required to handle cts.Cancel()
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
