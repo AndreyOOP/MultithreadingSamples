589f36a980b7d3f8810b1078297b8b1675b92147
abc <abc@gmail.com>
2022-01-20 22:21:13 +0200

    Thread local storage

---
+++ MultithreadingSamples/5. ParallelLoops/ThreadLocalStorage.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingSamples._5._ParallelLoops
+{
+    [TestClass]
+    public class ThreadLocalStorage
+    {
+        [TestMethod]
+        public void NoThreadLocalStorage()
+        {
+            int sum = 0;
+            Parallel.For(1, 1001, i =>
+            {
+                // the problem here is a lot of calls to Interlocked which decrease performance
+                Interlocked.Add(ref sum, i);
+            });
+            Console.WriteLine($"Total {sum}");
+        }
+
+        [TestMethod]
+        public void ThreadLocalStorageTest()
+        {
+            int sum = 0;
+            Parallel.For(1, 1000001, // actually Parallel.For<T>
+                () => 0, // this defines which local storage varaible will be used
+                (i, state, tls) => {
+                    return ++tls; // thread safe operation, tls is local for thread
+                },
+                tlsValue => {
+                    Interlocked.Add(ref sum, tlsValue);
+                    Console.WriteLine($"Task {Task.CurrentId} updated value of sum to {sum}.");
+                }
+            );
+            Console.WriteLine($"Total {sum}");
+        }
+    }
+}
